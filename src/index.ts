7694248a16e9d837adcb3c24297e5e0a50aac224
minhcong <83624415+dmcong@users.noreply.github.com>
2022-06-08 15:36:42 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const helloSentreWeb3 = () => {
+  console.log("hello");
+};
---
+++ test/example.test.ts
@@ -0,0 +1,11 @@
+import { helloSentreWeb3 } from "./../src/index";
+
+describe("balancer-amm", () => {
+  before("Is generate data!", async () => {
+    // ToDo:
+  });
+
+  it("Is initialized pool!", async () => {
+    helloSentreWeb3();
+  });
+});
